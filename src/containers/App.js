155f9bc27363bac8adcb1415816439115a4176a6
Jazz901 <jsdaugs@gmail.com>
2021-01-08 17:23:45 +0100

    chanegd project using state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CardList from '../components/CardList'; 
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -7,28 +7,64 @@ import './App.css';
 
 
 
-class App extends React.Component {
-    constructor(){
+const App = () => {
+
+    //for class component
+    /*constructor(){
         super()
         this.state = {
             robots: [],
             searchfield: ''
         }
+    }*/
+
+    const [robots, setRobots] = useState([]); 
+    const [searchfield, setSearchField] = useState('');
+
+    const fetchRobots = async() => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await resp.json()
+        setRobots(data)
     }
 
-    componentDidMount(){
+    // for class component
+    /*componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response=> Response.json())
         .then(users => this.setState({robots: users}));          
-    }
+    }*/
+
+    useEffect(() => {
+        fetchRobots()
+    }, [])
      
     
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
     }
 
-    render() {
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+     })
+
+    return(
+        <div>
+            {
+                robots.length === 0 ? <h1>Loading</h1> :
+                 <div className="tc">
+                    <h1 className="f1">Robo Friends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <CardList robots={ filteredRobots }/>
+                    </Scroll>
+                 </div>
+            }
+        </div>
+    ); 
+
+    // for class component
+    /*render() {
         const { robots, searchfield} = this.state; 
         const filteredRobots = robots.filter(robot =>{
            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
@@ -50,7 +86,7 @@ class App extends React.Component {
         }
        
 
-    }
+    }*/
    
 }   
 
