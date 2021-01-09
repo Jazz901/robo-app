079226e3fd01acd6f902420d5afc2398ddf6ffa8
Jazz901 <jsdaugs@gmail.com>
2021-01-09 21:50:35 +0100

    Updates

---
+++ precache-manifest.24a4902cf4c1c5f4b2edf8d41c73ff98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4faae22b0f27b47ed0d3f013ad24db",
+    "url": "/robo-app/index.html"
+  },
+  {
+    "revision": "f38cec07b9826f972c34",
+    "url": "/robo-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "306bead9371e211fb393",
+    "url": "/robo-app/static/css/main.48e6b84d.chunk.css"
+  },
+  {
+    "revision": "f38cec07b9826f972c34",
+    "url": "/robo-app/static/js/2.0cb25969.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/robo-app/static/js/2.0cb25969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306bead9371e211fb393",
+    "url": "/robo-app/static/js/main.d6c564d0.chunk.js"
+  },
+  {
+    "revision": "bc719707459dd84dfb39",
+    "url": "/robo-app/static/js/runtime-main.2e83a451.js"
+  }
+]);
